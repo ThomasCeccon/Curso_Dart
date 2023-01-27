284d1d4b5f13d5a12e3592da8f2abc083c64d0fb
Thomas Ceccon <cecconthomas275@gmail.com>
2023-01-26 23:15:51 -0300

    segundo commit

--- Anotações/revisao/compras/lista_compras.dart
+++ Anotações/revisao/compras/lista_compras.dart
@@ -1,40 +1,23 @@
 import 'dart:io';
 
-main(){
-
+main() {
 //declaracao das variaveis
-var produtos=[];
-var input_text;
-bool condicao=true;
+  var produtos = [];
+  var input_text;
+  bool condicao = true;
 
 //entrada de dados
-while(condicao){
+  while (condicao) {
+    print("Entre com os produtos");
+    input_text = stdin.readByteSync();
 
-  print("Entre com os produtos");
-  input_text=stdin.readByteSync();
-
-  if(input_text=="sair"){
-    condicao=false;
-    print("Programa terminado");
-  }
-  else if(input_text=="imprimir"){
+    if (input_text == "sair") {
+      condicao = false;
+      print("Programa terminado");
+    } else if (input_text == "imprimir") {
       print(produtos);
-  }
-  else{
+    } else {
       produtos.add(input_text);
-      
     }
-
-
-}
-
-
-//processamento
-
-
-
-//saida de dados
-
-
-
+  }
 }
--- codigos/variaveis/var.dart
+++ codigos/variaveis/var.dart
@@ -1,11 +1,13 @@
-import 'dart:ffi';
+import 'dart:io';
 
-main(){
-  var hello="hello word";
-   hello="outro valor";//vai mostrar esse,seque a ordem
-  var numero=100;
-   numero=90;
- 
+main() {
+  //declaracao das variaveis
+  var hello = "hello word";
+  hello = "outro valor"; //vai mostrar esse,seque a ordem
+  var numero = 10;
+  numero = 90;
+
+  //saida de dados
   print(hello);
   print(numero);
 }
